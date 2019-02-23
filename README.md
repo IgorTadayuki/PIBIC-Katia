b204f1916bd811f0901a5447f22c694d0bea143f
Phelipe Alves <sougyn@gmail.com>
2019-02-23 14:12:45 -0300

    Updating the project

--- README.md
+++ README.md
@@ -4,18 +4,18 @@
 
 Install mininet
 ```
-pip install git+https://github.com/mininet/mininet.git
+$ pip install git+https://github.com/mininet/mininet.git
 ```
 
 Install all dependencies
 ```
-pip install -r requirements.txt
+$ pip install -r requirements.txt
 ```
 
 ### Debugging the code
 To debug the application, run the app.py file as the main application
 ```
-python app.py
+$ python app.py
 ```
 or configure to use your IDE: 
 1. https://www.jetbrains.com/help/pycharm/creating-and-running-your-first-python-project.html
@@ -26,9 +26,70 @@ or configure to use your IDE:
 
 ![alt text](https://raw.githubusercontent.com/LABORA-INF-UFG/PIBIC-Katia/master/docs/topology.jpg)
 
+**Obs.:** To each scenario use the **OpenFlow protocol version 1.3**
+
 ### Simple Scenario (scenario1)
 
 Mininet simple scenario (scenario1):
 ```
-mn --custom topology/simple_scenario.py --topo=simple_scenario_topo
+$ mn --custom topology/simple_scenario.py --topo=simple_scenario_topo
 ```
+
+Run in your host the following command to show all rules added on switch `s1`: 
+```
+$ ovs-ofctl dump-flows s1 -O OpenFlow13
+```
+We wait have two rules:
+```
+OFPST_FLOW reply (OF1.3) (xid=0x2):
+ cookie=0x0, duration=58.695s, table=0, n_packets=3, n_bytes=294, priority=1,in_port=1,dl_dst=00:00:00:00:00:02 actions=output:2
+ cookie=0x0, duration=58.688s, table=0, n_packets=3, n_bytes=294, priority=1,in_port=2,dl_dst=00:00:00:00:00:01 actions=output:1
+ cookie=0x0, duration=66.593s, table=0, n_packets=18, n_bytes=1484, priority=0 actions=CONTROLLER:65535
+```
+
+and on switch `s2`:
+```
+OFPST_FLOW reply (OF1.3) (xid=0x2):
+ cookie=0x0, duration=232.293s, table=0, n_packets=3, n_bytes=294, priority=1,in_port=1,dl_dst=00:00:00:00:00:02 actions=output:2
+ cookie=0x0, duration=232.290s, table=0, n_packets=3, n_bytes=294, priority=1,in_port=2,dl_dst=00:00:00:00:00:01 actions=output:1
+ cookie=0x0, duration=240.163s, table=0, n_packets=21, n_bytes=1694, priority=0 actions=CONTROLLER:65535
+```
+
+**Obs.:** Note that we have one rule for the output packet and one for the input packet, i.e., ping response (e.g. h1 ping h2). 
+```
+(h1 -> s1 -> s2 -> h2)
+(h2 -> s2 -> s1 -> h1)
+```
+
+To see the number of each port on the switch, run:
+```
+$ ovs-ofctl show s1 -O OpenFlow13
+```
+
+```
+OFPT_FEATURES_REPLY (OF1.3) (xid=0x2): dpid:0000000000000001
+n_tables:254, n_buffers:256
+capabilities: FLOW_STATS TABLE_STATS PORT_STATS GROUP_STATS QUEUE_STATS
+OFPST_PORT_DESC reply (OF1.3) (xid=0x3):
+ 1(s1-eth1): addr:92:88:42:34:65:af
+     config:     0
+     state:      0
+     current:    10GB-FD COPPER
+     speed: 10000 Mbps now, 0 Mbps max
+ 2(s1-eth2): addr:d2:9e:ac:8d:50:9a
+     config:     0
+     state:      0
+     current:    10GB-FD COPPER
+     speed: 10000 Mbps now, 0 Mbps max
+ LOCAL(s1): addr:be:4b:f6:45:be:47
+     config:     PORT_DOWN
+     state:      LINK_DOWN
+     speed: 0 Mbps now, 0 Mbps max
+OFPT_GET_CONFIG_REPLY (OF1.3) (xid=0x5): frags=normal miss_send_len=0
+```
+
+## Now implement the scenario1 in file [./controller/simple_scenario.py](./controller/simple_scenario.py)
+
+To do this, change line 12 of [app.py](./app.py) file to reference the file [./controller/simple_scenario.py](./controller/simple_scenario.py)
+
+
--- topology/simple_scenario.py
+++ topology/simple_scenario.py
@@ -1,22 +1,12 @@
 #!/usr/bin/python
 
-"""
-This example creates a multi-controller network from semi-scratch by
-using the net.add*() API and manually starting the switches and controllers.
-
-This is the "mid-level" API, which is an alternative to the "high-level"
-Topo() API which supports parametrized topology classes.
-
-Note that one could also create a custom switch class and pass it into
-the Mininet() constructor.
-"""
-
-from mininet.net import Mininet
-from mininet.node import Controller, OVSSwitch, RemoteController
 from mininet.cli import CLI
-from mininet.log import setLogLevel, info
+from mininet.log import info
+from mininet.net import Mininet
+from mininet.node import OVSSwitch, RemoteController
 
-"""Create simple scenario
+"""
+Create simple scenario
 
 Adding the 'topos' dict with a key/value pair to generate our newly defined
 topology enables one to pass in '--topo=simple_scenario_topo' from the command line.
@@ -29,13 +19,14 @@ class SimpleScenarioTopo(Topo):
 
     def __init__(self):
         """
-        Create custom topo to lab 08.
+        Create custom topo SimpleScenarioTopo.
         """
 
         # Initialize topology
         Topo.__init__(self)
 
-        # protocols = "OpenFlow10"
+        info("*** Using Openflow 1.3 \n")
+        protocols = "OpenFlow13"
 
         net = Mininet(switch=OVSSwitch)
 
@@ -43,12 +34,12 @@ class SimpleScenarioTopo(Topo):
         c1 = net.addController('c1', controller=RemoteController, ip='127.0.0.1', port=6633)
 
         info("*** Creating switches\n")
-        s1 = net.addSwitch('s1')
-        s2 = net.addSwitch('s2')
+        s1 = net.addSwitch('s1', protocols=protocols)
+        s2 = net.addSwitch('s2', protocols=protocols)
 
         info("*** Creating hosts\n")
-        h1 = net.addHost(name='h1', ip='10.0.0.1', defaultHost='')
-        h2 = net.addHost(name='h2', ip='10.0.0.2', defaultHost='')
+        h1 = net.addHost(name='h1', ip='10.0.0.1', mac='00:00:00:00:00:01', defaultHost='')
+        h2 = net.addHost(name='h2', ip='10.0.0.2', mac='00:00:00:00:00:02', defaultHost='')
 
         info("*** Creating links\n")
         net.addLink(h1, s1)
@@ -65,49 +56,3 @@ class SimpleScenarioTopo(Topo):
 
 
 topos = {'simple_scenario_topo': (lambda: SimpleScenarioTopo())}
-
-
-# def SimpleScenario():
-#     "Create a network from semi-scratch with multiple controllers."
-#
-#     net = Mininet(controller=Controller, switch=OVSSwitch)
-#
-#     info("*** Creating (reference) controllers\n")
-#     c1 = net.addController('c1', port=6633)
-#     c2 = net.addController('c2', port=6634)
-#
-#     info("*** Creating switches\n")
-#     s1 = net.addSwitch('s1')
-#     s2 = net.addSwitch('s2')
-#
-#     info("*** Creating hosts\n")
-#     hosts1 = [net.addHost('h%d' % n) for n in (3, 4)]
-#     hosts2 = [net.addHost('h%d' % n) for n in (5, 6)]
-#
-#     info("*** Creating links\n")
-#     for h in hosts1:
-#         net.addLink(s1, h)
-#     for h in hosts2:
-#         net.addLink(s2, h)
-#     net.addLink(s1, s2)
-#
-#     info("*** Starting network\n")
-#     net.build()
-#     c1.start()
-#     c2.start()
-#     s1.start([c1])
-#     s2.start([c2])
-#
-#     info("*** Testing network\n")
-#     net.pingAll()
-#
-#     info("*** Running CLI\n")
-#     CLI(net)
-#
-#     info("*** Stopping network\n")
-#     net.stop()
-#
-#
-# if __name__ == '__main__':
-#     setLogLevel('info')  # for CLI output
-#     SimpleScenario()
