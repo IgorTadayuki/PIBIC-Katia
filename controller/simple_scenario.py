dfd48bd8197419a577b69d50f7e91c810966a2e2
Phelipe Alves <sougyn@gmail.com>
2019-02-28 08:31:11 -0300

    Update the class name

--- controller/simple_scenario.py
+++ controller/simple_scenario.py
@@ -33,11 +33,11 @@ TOPOLOGY = {
 }
 
 
-class SimpleSwitch13(app_manager.RyuApp):
+class SimpleScenario13(app_manager.RyuApp):
     OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
 
     def __init__(self, *args, **kwargs):
-        super(SimpleSwitch13, self).__init__(*args, **kwargs)
+        super(SimpleScenario13, self).__init__(*args, **kwargs)
         self.mac_to_port = {}
 
     @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
